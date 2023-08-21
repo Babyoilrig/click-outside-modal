fffe68f8134ac55246310b1ba641ea97fe95fac1
babyoilrig <catrin.mentzoni@yahoo.com>
2023-08-21 16:45:34 +0100

    Adding html to the modal

--- click-outside.js
+++ click-outside.js
@@ -6,7 +6,7 @@ const cardButtons = document.querySelectorAll('.card button');
 
 //Selecting modal
 const modalInner = document.querySelector('.modal-inner');
-console.log(modalInner);
+// console.log(modalInner);
 
 function handleCardButtonClick() {
     const button = event.currentTarget;
@@ -16,11 +16,14 @@ function handleCardButtonClick() {
     const imgSrc = card.querySelector('img').src;
     //Grabbing the card description
     const desc = card.dataset.description;
-    console.log(desc);
+    // console.log(desc);
+    const name = card.querySelector('h2').textContent;
     //Populate the modal with the new info
-
-
-
+    modalInner.innerHTML = 
+    `
+    <img src="${imgSrc.replace('200', '600')}" alt="${name}"/>
+    <p>${desc}</p>
+    `
 }
 //Looping over the card buttons and adding an event listener
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
