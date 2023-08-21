4eb9f6e9612163f8297e6af6258952448e591dd2
babyoilrig <catrin.mentzoni@yahoo.com>
2023-08-21 16:39:48 +0100

    Grabbing the modal inner and console logging it

--- click-outside.js
+++ click-outside.js
@@ -4,6 +4,10 @@ console.log('It Works!')
 const cardButtons = document.querySelectorAll('.card button');
 // console.log(cardButtons);
 
+//Selecting modal
+const modalInner = document.querySelector('.modal-inner');
+console.log(modalInner);
+
 function handleCardButtonClick() {
     const button = event.currentTarget;
     //Finding the closest card to the button
@@ -13,6 +17,9 @@ function handleCardButtonClick() {
     //Grabbing the card description
     const desc = card.dataset.description;
     console.log(desc);
+    //Populate the modal with the new info
+
+
 
 }
 //Looping over the card buttons and adding an event listener
