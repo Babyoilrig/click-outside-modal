42496cdf3b2c342eefdcca82af31d6a1bdcb4aff
babyoilrig <catrin.mentzoni@yahoo.com>
2023-08-21 16:11:43 +0100

    Selecting the buttons and looping over each to add an event listener

--- click-outside.js
+++ click-outside.js
@@ -1,1 +1,7 @@
 console.log('It Works!')
+
+//Selecting card buttons
+const cardButtons = document.querySelectorAll('.card button');
+// console.log(cardButtons);
+
+cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
