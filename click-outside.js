b0283d96e7d3552bf639d7d94b35eb68cbbf6f9d
babyoilrig <catrin.mentzoni@yahoo.com>
2023-08-21 16:15:22 +0100

    Adding console log to make sure the button click functionality is working

--- click-outside.js
+++ click-outside.js
@@ -4,4 +4,8 @@ console.log('It Works!')
 const cardButtons = document.querySelectorAll('.card button');
 // console.log(cardButtons);
 
+function handleCardButtonClick() {
+    console.log('YA CLICKED IT!')
+}
+
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
